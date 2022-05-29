d497d15df3c370140b12e2ae149ef313fe108b48
Prashant kumar <104028213+prashantkumar1122@users.noreply.github.com>
2022-05-30 01:08:45 +0530

    Create Change value of a key in a nested dictionary.py

---
+++ Dictionary/Change value of a key in a nested dictionary.py
@@ -0,0 +1,7 @@
+sample= {'emp1': {'name': 'Jhon', 'salary': 7500},
+'emp2': {'name': 'Emma', 'salary': 8000},
+'emp3': {'name': 'Brad', 'salary': 500}}
+
+sample['emp3']['salary']=100000
+
+print(sample)
