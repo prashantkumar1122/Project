80cf95a13e684a2645339b6171cdb6f231b48b11
Prashant kumar <104028213+prashantkumar1122@users.noreply.github.com>
2022-05-30 01:20:09 +0530

    Create Fing SI by user define function.py

---
+++ Function/Fing SI by user define function.py
@@ -0,0 +1,9 @@
+def si(p,r,t):
+  return (p*r*t)/100
+p=int(input("entre the principle rate:"))
+      
+r=int(input("entre the  rate:"))      
+
+t=int(input("entre the time :"))
+print("simple intrest is:")  
+print(si(p,r,t))
