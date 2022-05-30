c92720a9191a08ade0f0babd65c7d1e97df314a2
Prashant kumar <104028213+prashantkumar1122@users.noreply.github.com>
2022-05-30 17:51:01 +0530

    Create Concatination of two arraay by verticaaly and horzontally.py

---
+++ Numpy/Concatination of two arraay by verticaaly and horzontally.py
@@ -0,0 +1,10 @@
+# concatination of two array by vertically
+
+import numpy as np
+ar=np.array([1,2,3])
+a=np.array([2,3,4])
+print(np.vstack((ar,a)))
+
+#concatination of two array by horizontally
+
+print(np.hstack((ar,a)))
