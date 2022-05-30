40bded0d92bb907d414066faf3fe070b7f19557b
Prashant kumar <104028213+prashantkumar1122@users.noreply.github.com>
2022-05-30 17:37:09 +0530

    Create Find volume and area of cylinder.py

---
+++ File handling and Modules/Find volume and area of cylinder.py
@@ -0,0 +1,10 @@
+pi=22/7 
+r=float(input("enter the radius of cylinder:"))
+h=float(input("enter the  height of cylinder:"))
+print("volume of cylinder is:")
+vol=(pi*r**2)*h 
+print(vol)
+
+print("Area of cylinder is:")
+area=(2*pi*r)*(r+h)
+print(area)
